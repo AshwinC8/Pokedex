5f82a2627eee7238b780ecb7a6ef4b3e7017ab3e
AshwinC8 <ashwinsam01@yahoo.com>
2021-10-03 18:41:39 +0530

    Pokemon reactive search bar

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,31 @@
 <script>
     import {pokemon} from "../stores/pokestore";
     import PokemanCard from "../components/pokemanCard.svelte"
+
+    let searchTerm = "";
+    let filterPokemon = [];
+
+    $: {
+        console.log(searchTerm);
+
+        if(searchTerm){
+          //search pokemon
+          filterPokemon = $pokemon.filter(pokeman => pokeman.name.toLowerCase().includes(searchTerm.toLowerCase()));
+        }else {
+          filterPokemon = [... $pokemon]
+        }
+    }
+
 </script>
 
 <svelte:head>
     <title>SwelteKit Pokedex</title>
 </svelte:head>
-<h1 class="text-4xl text-center my-8 uppercase" >Svelte kit Pokedex</h1>
 
-<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">  
-  {#each $pokemon as pokeman}
+<h1 class="text-4xl text-center my-8 uppercase" >Svelte kit Pokedex</h1>
+<input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" type="text" bind:value={searchTerm} placeholder="Search Pokemon" > 
+<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1" >  
+  {#each filterPokemon as pokeman}
     <PokemanCard pokeman={pokeman}/>
   {/each}
 </div>
